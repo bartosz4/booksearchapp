073e49ea8652c84c1ec925cf64ef8f1dec99c211
bartosz4 <bartosz44@gmail.com>
2019-04-05 14:00:18 +0200

    Search added

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,2 +1,19 @@
 import React from 'react';
 
+const SearchArea =() =>{
+    return(
+        <div className="container">
+            <div className="row">
+                <section className="col s4 offset-s4">
+                    <form action="">
+                        <div className="input-field">
+                            <input placeholder="Search book" type="text"/>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default SearchArea;
