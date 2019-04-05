1bf7f24b07cd0d39da0cc87c475b3376f9ab8b5f
bartosz4 <bartosz44@gmail.com>
2019-04-05 14:37:44 +0200

    working search bar and connecting to API

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,37 @@ import Nav from './Nav';
 import SearchArea from './SearchArea';
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            books: [],
+            searchWord: ''
+        }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchWord}`)
+            .then(data => data.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    books: [...data.items]
+                })
+            })
+    }
+
+    handleChange =(e) =>{
+        this.setState({
+            searchWord: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <Nav/>
-                <SearchArea/>
+                <SearchArea handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
             </div>
         );
     }
