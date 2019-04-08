59a88336f5cdf0a61d10a319bd6b08c347b7b4fc
bartosz4 <bartosz44@gmail.com>
2019-04-08 23:36:30 +0200

    Some styling and conf gh-pages

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Book = (props) => {
     return(
-            <div className="col s12 m6 l4">
+            <div className="col s12 m6 l3">
                 <div className="z-depth-2">
-                    <div className="card">
+                    <div className="card large hoverable">
                         <div className="card-image ">
                         {
-                             props.image == null ? <img src={`http://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt={props.title}/> : <img src={props.image.thumbnail} alt={props.title}/>
+                             props.image == null ? <img src={`http://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt={props.title}/> : <img className="responsive-img" src={props.image.thumbnail} alt={props.title}/>
 
                         }
                         </div>
                         <div className="card-content">
                             <h6 className="card-title">{props.title}</h6>
-                            <p>{props.description}</p>
+                            <p className="truncate">{props.description}</p>
                         </div>
                     </div>
                 </div>
